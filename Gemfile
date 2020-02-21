19ce363dffcd09848e103a9d152c74696d60b751
Leonardt Lauenstein <leonardt@outlook.com>
2020-02-21 17:08:22 +0000

    indo

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
 
+gem 'rest-client'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
@@ -26,7 +27,7 @@ gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
+# gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+require 'open-uri'
+
+# require 'net/http'
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# uri = URI(url)
+# response = Net::HTTP.get(uri)
+# JSON.parse(response)
+
+francisco = RestClient.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+ing = JSON.parse(francisco.body)
+
+ing.each do |array|
+  array[1].each do |el|
+   Ingredient.create(name: el["strIngredient1"])
+  end
+end
+
+# info = {numb: '1'}
+
+
+# array = [false, true]
+# array[0]
+
+
+# facebook = {
+#   users: {
+#     "francisco": 23,
+#     "joao": 32
+#   }
+# }
+
+# facebook[:users]["francisco"]
+
